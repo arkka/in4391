1da2f8392c9641dce014f2030f698115b87702c5
sukmawicaksana <sukmawicaksana@gmail.com>
2016-04-07 16:47:45 +0200

    Fix unit dead

--- src/nl/tudelft/in4391/da/Arena.java
+++ src/nl/tudelft/in4391/da/Arena.java
@@ -16,10 +16,15 @@ public class Arena implements Serializable {
     public ArrayList<Unit> dragons = new ArrayList<Unit>();
 
     // Get player Unit
-    public Unit getMyUnit(Player p){
-        Unit u = p.getUnit();
-        int i = units.indexOf(u);
-        return units.get(i);
+    public synchronized Unit getMyUnit(Player p){
+        Unit u = null;
+        int i = units.indexOf(p.getUnit());
+	    try {
+		    u = units.get(i);
+	    } catch(Exception e) {
+
+	    }
+        return u;
     }
 
     // Sync data
@@ -88,15 +93,6 @@ public class Arena implements Serializable {
         }
     }
 
-    public boolean checkSurrounding(Unit unit, int x, int y) {
-        if (unitCell[x][y] != null){
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-
     // After check whether surrounding empty or not when moving
     // If not, check the type of unit
     // Do damage if dragon
@@ -117,15 +113,17 @@ public class Arena implements Serializable {
 
 	public synchronized void healUnit(Unit source, Unit target){
 		if ((Math.abs(source.getX() - target.getX()) <= 5) && (Math.abs(source.getY() - target.getY()) <= 6)){
-			if (target.getHitPoints() <= 0.5 * target.getMaxHitPoints() ){
-				target.setHitPoints(target.getHitPoints() + source.getHitPoints());
-			}
+			target.setHitPoints(target.getHitPoints() + source.getHitPoints());
+//			if (target.getHitPoints() <= 0.5 * target.getMaxHitPoints() ){
+//				target.setHitPoints(target.getHitPoints() + source.getHitPoints());
+//			}
 		}
 	}
 
 	public synchronized void attackUnit(Unit source, Unit target){
 		if ((Math.abs(source.getX() - target.getX()) <= 1) && (Math.abs(source.getY() - target.getY()) <= 1)){
 			target.setHitPoints(target.getHitPoints() - source.getHitPoints());
+			unitCell[target.getX()][target.getY()] = target;
 			checkDead(target);
 		}
 	}
@@ -136,6 +134,8 @@ public class Arena implements Serializable {
 
 	        if(unit.getType().equals("Dragon")) dragons.remove(unit);
 	        else knights.remove(unit);
+
+	        unit.setDead();
 
 	        units.remove(unit);
         }
--- src/nl/tudelft/in4391/da/GameBot.java
+++ src/nl/tudelft/in4391/da/GameBot.java
@@ -5,8 +5,8 @@ package nl.tudelft.in4391.da;
  */
 
 public class GameBot {
-	private static Integer MAX_KNIGHT = 100;
-	private static Integer MAX_DRAGON = 20;
+	private static Integer MAX_KNIGHT = 50;
+	private static Integer MAX_DRAGON = 5;
 
 	public GameBot() {
 
@@ -16,13 +16,15 @@ public class GameBot {
 	public static void main(String[] args)
 	{
 
+		for(int j=0;j<MAX_DRAGON;j++) {
+			new Bot("Dragon "+(j+1),"Dragon").start();
+		}
+
 		for(int i=0;i<MAX_KNIGHT;i++) {
 			new Bot("Knight "+(i+1),"Knight").start();
 		}
 
-		for(int j=0;j<MAX_DRAGON;j++) {
-			new Bot("Dragon "+(j+1),"Dragon").start();
-		}
+
 
 	}
 
